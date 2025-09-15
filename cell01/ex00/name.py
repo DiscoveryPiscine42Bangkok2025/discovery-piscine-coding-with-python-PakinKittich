50209d980d46b41670d05bf00923ab5ab8ae5332
unknown <65070173@kmitl.ac.th>
2025-09-15 23:56:34 +0700

    all

---
+++ cell01/ex00/name.py
@@ -0,0 +1,3 @@
+first_name = 'Pakin'
+last_name = 'Kittichaikulkit'
+print(first_name, last_name)
---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = 'Pakin'
+last_name = 'Kittichaikulkit'
+whole_name = first_name + ' ' + last_name
+print(whole_name)
